4fa61cd38e1c79a2826ba9a60db7fd19508291c1
Egor <egormaxb@gmail.com>
2023-11-02 20:16:33 -0700

    Register page and form component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/Login';
 import HomePage from './pages/Home';
+import RegisterPage from './pages/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
     </Router>
