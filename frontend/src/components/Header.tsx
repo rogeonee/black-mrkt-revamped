ddd98a3c1fecbb7e85293f09ee49bc60e3044ab9
Egor <egormaxb@gmail.com>
2023-11-02 21:17:48 -0700

    Header styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 const Header = () => {
-  const isLoggedIn = false;  // Hardcoded for now
+  const isLoggedIn = true;  // Hardcoded for now
   
 // <Link to="/" className={styles.logo}>Website Logo</Link>
 
-  return (
+return (
     <header className={styles.headerContainer}>
-      
+      <Link to="/" className={styles.logo}>Website Logo</Link>
       <nav className={styles.navLinks}>
         <Link to="/home">Home</Link>
         <Link to="/cart">Shopping Cart</Link>
@@ -17,8 +17,8 @@ const Header = () => {
           isLoggedIn 
             ? (
               <>
-                <Link to="/account" onClick={() => console.log('Your account')}>Username</Link>
-                <Link to="/login" onClick={() => console.log('Logging out...')}>Log Out</Link>
+                <Link to="/account" onClick={() => console.log('Your account')}>My Account</Link>
+                <Link to="/login" className={styles.logout} onClick={() => console.log('Logging out...')}>Log Out</Link>
               </>
             ) 
             : (
